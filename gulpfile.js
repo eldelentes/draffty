e7adec2774436e62f887a7fe23d8b48b4e60e080
Eduardo Gutierrez <eldelentes@egmail.com>
2016-07-25 11:34:23 -0500

    Add gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	connect = require('gulp-connect');
 
-gulp.task('default', function() {
+gulp.task('connect', function() {
+  connect.server({
+    root: './',
+    livereload: true
+  });
 });
 
 gulp.task('autoprefixer', function(){
-	return gulp.src('client/app.css')
+	return gulp.src('./css/*.css')
 		.pipe(autoprefixer({
-		    browsers: ['last 2 versions'],
+		    browsers: ['last 3 versions'],
 		    cascade: false
 		}))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('./css'))
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass', 'autoprefixer']);
+});
+
+gulp.task('default', ['watch', 'connect']);
