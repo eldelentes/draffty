9d7e08ae167fed26e359313c28c2f6f47382db10
Eduardo Gutierrez <eldelentes@egmail.com>
2016-07-25 10:51:29 -0500

    Add basic setup files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
+});
+
+gulp.task('autoprefixer', function(){
+	return gulp.src('client/app.css')
+		.pipe(autoprefixer({
+		    browsers: ['last 2 versions'],
+		    cascade: false
+		}))
+		.pipe(gulp.dest('dist'));
 });
